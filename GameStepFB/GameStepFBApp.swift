b309f812935745487f782dbc5f727a60da4ac40b
Ramsey <renzhix@andrew.cmu.edu>
2022-10-27 15:11:10 -0400

    Initial Commit

---
+++ GameStepFB/GameStepFBApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStepFBApp.swift
+//  GameStepFB
+//
+//  Created by 荀任之 on 2022/10/27.
+//
+
+import SwiftUI
+
+@main
+struct GameStepFBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
