d29eba724e2dc300ff6ebc56db246122d59ea52c
haox1228 <48774325+haox1228@users.noreply.github.com>
2022-11-10 11:04:02 -0500

    added plist

--- GameStepFB/ViewModel.swift
+++ GameStepFB/ViewModel.swift
@@ -31,20 +31,38 @@ struct User: Identifiable, Codable {
 }
 
 class ViewModel: ObservableObject {
-  private let store = Firestore.firestore()
-  @Published var user: User
-  init() {
-    self.user = User(name: "Ramsey", password: "123456", coins: 100, totalSteps: 10000)
-    add(self.user)
-  }
-  func add(_ user: User) {
-    do {
-      let newUser = user
-      _ = try store.collection("test").addDocument(from: newUser)
-      print("added to database")
-    } catch {
-      fatalError("Unable to add book: \(error.localizedDescription).")
-    }
-  }
+	private var healthStore: HealthStore?
+	private let store = Firestore.firestore()
+	@Published var user: User
+	init() {
+		self.user = User(name: "Nick", password: "123456", coins: 200, totalSteps: 0)
+		self.user.totalSteps = Int(updateSteps())
+		add(self.user)
+		}
+			
+			func add(_ user: User) {
+				do {
+					let newUser = user
+					_ = try store.collection("test").addDocument(from: newUser)
+					print("added to database")
+				} catch {
+					fatalError("Unable to add book: \(error.localizedDescription).")
+				}
+			}
+	func updateSteps()->Double{
+		var currentStep: Double = 0
+		if let healthStore = healthStore{
+			healthStore.requestAuthorization { success in
+				if success {
+					healthStore.getSteps { unwrapp in
+						currentStep = unwrapp
+					}
+				}
+			}
+		}
+		return currentStep
+	}
+			
+			
+	
 }
-
